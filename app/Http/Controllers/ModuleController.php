57a3d96db45509fdacbe48887d9c1dad46d9b462
darciosoares <darciosoares28@gmail.com>
2023-05-17 16:20:49 -0300

    Criação do Module -> modulo  dos cursos -> criação do modulo, resource, controller, migrate e da rota na api.

---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\ModuleResource;
+use App\Models\Module;
+
+class ModuleController extends Controller
+{
+    public function index( string $id)
+    {
+
+        $modules = Module::where('course_id', $id)->get();
+
+        return ModuleResource::collection($modules);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ModuleController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/courses',[CourseController::class,'index']);
 
 Route::get('/courses/{id}',[CourseController::class,'store']);
+
+Route::get('/courses/{id}/modules',[ModuleController::class,'index']);
 
 
 Route::get('/', function(){
